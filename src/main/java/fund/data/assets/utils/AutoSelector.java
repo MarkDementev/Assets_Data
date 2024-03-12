f197ed851de00ee94cb773d49bea59d87fe51876
MarkDementev <mark_sur@mail.ru>
2024-03-12 12:07:33 +0300

    add selectTaxSystem method in AutoSelector

--- src/main/java/fund/data/assets/utils/AutoSelector.java
+++ src/main/java/fund/data/assets/utils/AutoSelector.java
@@ -1,12 +1,25 @@
 package fund.data.assets.utils;
 
+import fund.data.assets.model.assets.exchange.FixedRateBond;
 import fund.data.assets.utils.enums.AssetCurrency;
 import fund.data.assets.utils.enums.CommissionSystem;
 import fund.data.assets.utils.enums.TaxSystem;
 
-public class AutoSelector {
-    public static TaxSystem selectTaxSystem(AssetCurrency assetCurrency, String assetTypeName) {
+import static fund.data.assets.utils.enums.AssetCurrency.RUSRUB;
 
+public class AutoSelector {
+    public static String NOT_IMPLEMENTED_CURRENCY = "Sorry, this currency is not yet supported by the fund.";
+    public static String NOT_IMPLEMENTED_ASSET_TYPE_TAX_SYSTEM = "Sorry, this asset type tax system is not yet" +
+            " supported by the fund.";
+
+    public static TaxSystem selectTaxSystem(AssetCurrency assetCurrency, String assetTypeName) {
+        if (assetCurrency.getTitle().equals(RUSRUB.getTitle())) {
+            if (assetTypeName.equals(FixedRateBond.class.getTypeName())) {
+                return TaxSystem.EQUAL_COUPON_DIVIDEND_TRADE;
+            }
+            throw new IllegalArgumentException(NOT_IMPLEMENTED_ASSET_TYPE_TAX_SYSTEM);
+        }
+        throw new IllegalArgumentException(NOT_IMPLEMENTED_CURRENCY);
     }
 
     public static CommissionSystem selectCommissionSystem(AssetCurrency assetCurrency, String assetTypeName,
