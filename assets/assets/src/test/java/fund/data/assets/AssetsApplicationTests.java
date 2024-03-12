40407609062d8bf6cdb774263446b71dc331922e
MarkDementev <mark_sur@mail.ru>
2024-03-12 12:07:33 +0300

    init project

---
+++ assets/assets/src/test/java/fund/data/assets/AssetsApplicationTests.java
@@ -0,0 +1,13 @@
+package fund.data.assets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
