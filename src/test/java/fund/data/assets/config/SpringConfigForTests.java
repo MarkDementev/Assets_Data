7d0a9fa97a5db81397ae9efe1bb64db709cc4dd2
MarkDementev <mark_sur@mail.ru>
2024-05-03 16:55:05 +0300

    delete unuseful now property

--- src/test/java/fund/data/assets/config/SpringConfigForTests.java
+++ src/test/java/fund/data/assets/config/SpringConfigForTests.java
@@ -3,14 +3,12 @@ package fund.data.assets.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.context.annotation.PropertySource;
 
 import static fund.data.assets.config.SpringConfigForTests.TEST_PROFILE;
 
 @Configuration
 @Profile(TEST_PROFILE)
 @ComponentScan(basePackages = "fund.data.assets")
-@PropertySource(value = "classpath:/test_config/application.yml")
 public class SpringConfigForTests {
     public static final String TEST_PROFILE = "test";
 }
