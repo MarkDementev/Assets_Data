5a198edf01489ed72f8f3b151538db41230a2b60
MarkDementev <mark_sur@mail.ru>
2024-07-05 16:04:08 +0300

    add FirstBuyFixedRateBondDTO

--- src/main/java/fund/data/assets/dto/asset/exchange/FirstBuyFixedRateBondDTO.java
+++ src/main/java/fund/data/assets/dto/asset/exchange/FirstBuyFixedRateBondDTO.java
@@ -1,4 +1,78 @@
 package fund.data.assets.dto.asset.exchange;
 
+import fund.data.assets.utils.enums.AssetCurrency;
+import fund.data.assets.utils.enums.CommissionSystem;
+
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.PositiveOrZero;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+/**
+ * DTO для обслуживания внесения в систему данных о первой покупке пакета бумаг данного выпуска облигаций.
+ * @version 0.0.1-alpha
+ * @author MarkDementev a.k.a JavaMarkDem
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FirstBuyFixedRateBondDTO {
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private AssetCurrency assetCurrency;
+
+    @NotBlank
+    private String assetTitle;
+
+    @NotNull
+    @Positive
+    private Integer assetCount;
+
+    @NotNull
+    @Size(min = 12, max = 12)
+    @Pattern(regexp = "^[A-Z]{2}[A-Z0-9]{9}[0-9]$")
+    private String iSIN;
+
+    @NotBlank
+    private String assetIssuerTitle;
+
+    @NotNull
+    private LocalDate lastAssetBuyDate;
+
+    @Enumerated(EnumType.STRING)
+    private CommissionSystem assetCommissionSystem;
+
+    @NotNull
+    @Positive
+    private Integer bondParValue;
+
+    @NotNull
+    @Positive
+    private Float purchaseBondParValuePercent;
+
+    @NotNull
+    @PositiveOrZero
+    private Float bondAccruedInterest;
+
+    @NotNull
+    @PositiveOrZero
+    private Float bondCouponValue;
+
+    @NotNull
+    @PositiveOrZero
+    private Integer expectedBondCouponPaymentsCount;
+
+    @NotNull
+    private LocalDate bondMaturityDate;
 }
--- src/main/java/fund/data/assets/service/impl/FixedRateBondServiceImpl.java
+++ src/main/java/fund/data/assets/service/impl/FixedRateBondServiceImpl.java
@@ -1,14 +1,26 @@
 package fund.data.assets.service.impl;
 
+import fund.data.assets.dto.asset.exchange.FirstBuyFixedRateBondDTO;
 import fund.data.assets.model.asset.exchange.FixedRateBond;
+import fund.data.assets.model.asset.relationship.AssetRelationship;
 import fund.data.assets.repository.FixedRateBondRepository;
 import fund.data.assets.service.FixedRateBondService;
 
+import fund.data.assets.utils.enums.AssetCurrency;
+import fund.data.assets.utils.enums.CommissionSystem;
+import fund.data.assets.utils.enums.TaxSystem;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.*;
 import lombok.RequiredArgsConstructor;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 /**
  * Реализация сервиса для обслуживания облигаций с фиксированным купоном.
@@ -31,10 +43,12 @@ public class FixedRateBondServiceImpl implements FixedRateBondService {
         return fixedRateBondRepository.findAll();
     }
 
-//    @Override
-//    public FixedRateBond firstBuyFixedRateBond(FixedRateBondDTO fixedRateBondDTO) {
-//        AtomicReference<FixedRateBond> atomicNewFixedRateBond = new AtomicReference<>(new FixedRateBond());
-//
-//        return null;
-//    }
+    @Override
+    public FixedRateBond firstBuyFixedRateBond(FirstBuyFixedRateBondDTO firstBuyFixedRateBondDTO) {
+        //Пропиши связь с AccountCash
+
+        AtomicReference<FixedRateBond> atomicNewFixedRateBond = new AtomicReference<>(new FixedRateBond());
+
+        return null;
+    }
 }
