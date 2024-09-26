05e40b57c491e8da19bdbbb677f87de95d632936
MarkDementev <mark_sur@mail.ru>
2024-09-26 14:45:44 +0300

    add partialBuyFixedRateBondPackage into FixedRateBondService

--- src/main/java/fund/data/assets/service/FixedRateBondService.java
+++ src/main/java/fund/data/assets/service/FixedRateBondService.java
@@ -3,6 +3,7 @@ package fund.data.assets.service;
 import fund.data.assets.dto.asset.exchange.AssetsOwnersCountryDTO;
 import fund.data.assets.dto.asset.exchange.FirstBuyFixedRateBondDTO;
 import fund.data.assets.dto.asset.exchange.FixedRateBondFullSellDTO;
+import fund.data.assets.dto.asset.exchange.FixedRateBondBuyDTO;
 import fund.data.assets.dto.asset.exchange.FixedRateBondPartialSellDTO;
 import fund.data.assets.model.asset.exchange.FixedRateBondPackage;
 
@@ -18,6 +19,7 @@ public interface FixedRateBondService {
     FixedRateBondPackage getFixedRateBond(Long id);
     List<FixedRateBondPackage> getFixedRateBonds();
     FixedRateBondPackage firstBuyFixedRateBond(FirstBuyFixedRateBondDTO firstBuyFixedRateBondDTO);
+    FixedRateBondPackage partialBuyFixedRateBondPackage(Long id, FixedRateBondBuyDTO fixedRateBondBuyDTO);
     FixedRateBondPackage partialSellFixedRateBondPackage(Long id,
                                                          FixedRateBondPartialSellDTO fixedRateBondPartialSellDTO);
     void sellAllPackage(Long id, FixedRateBondFullSellDTO fixedRateBondFullSellDTO);
